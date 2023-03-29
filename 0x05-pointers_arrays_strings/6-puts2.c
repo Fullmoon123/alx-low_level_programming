10a94edc37620a48a49e79aa6fdf427c30901aff
Fullmoon123 <shabi.fuad@gmail.com>
2023-03-28 17:08:09 -0700

    45

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  * puts2 - function to print only one character out of two
+ *   * starting with the first one
+ *    * @str: input
+ *     * Return: print
+ */
+
+void puts2(char *str)
+{
+int i = 0, len = 0;
+
+while (str[i++])
+len++;
+for (i = 0; i < len; i += 2)
+_putchar(str[1]);
+_putchar('\n');
+}
