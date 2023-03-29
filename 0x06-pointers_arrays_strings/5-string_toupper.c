fc38daacf97b12252c05f11e9eeb37fa110d7a27
Fullmoon123 <shabi.fuad@gmail.com>
2023-03-29 07:45:06 -0700

    this

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * * string_toupper - changes all lowercase letters to upper case
+ * * @s: string to be changed.
+ * * Return: pointer to the changed string.
+ */
+
+char *string_toupper(char *s)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+if (s[i] > 96 && s[i] < 123)
+s[i] -= 32;
+return (s);
+}
